fbefee18f68a8d998cd4e17d932aa566f7dd76b2
Charlie WIllis <cjwillis48@gmail.com>
2019-02-20 23:40:31 -0500

    Removed plugins since they wouldn't work with GitHub security.  Tried to implement timestring without plugin

--- _plugins/TimeFilter.rb
+++
@@ -1,9 +0,0 @@
-module Jekyll
-    module TimeFilter
-        def secondstohhmm(seconds)
-            Time.at(seconds).utc.strftime("%H:%M:%S")
-        end
-    end
-end
-
-Liquid::Template.register_filter(Jekyll::TimeFilter)
